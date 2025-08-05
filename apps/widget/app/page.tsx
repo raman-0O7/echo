65c844febf47d541dbd3d8838599803e0bdf7c80
Raman Kumar <rku.430@gmail.com>
2025-08-05 22:27:15 +0530

    01: Setup

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,13 +1,11 @@
 import { Button } from "@workspace/ui/components/button"
-import { Input } from "@workspace/ui/components/input"
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello apps/web</h1>
+        <h1 className="text-2xl font-bold">Hello apps/widget</h1>
         <Button size="sm">Button</Button>
-        <Input />
       </div>
     </div>
   )
